9e1ea2f8d3bfb279e02ff125250b46252f42e794
edgar macin <edgarmacin_1107@hotmail.com>
2020-07-25 17:11:06 -0500

    se agrego el auth y las dev para que cargue bootstrap y utilizr sass etc

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/recetas', 'RecetasController' );
 
+Auth::routes();
 
-
+//Route::get('/home', 'HomeController@index')->name('home');
